a4b3332ee88ea66e845d551037e860f09ab1add9
Marg1na1 <105055730+Marg1na1@users.noreply.github.com>
2022-12-09 13:32:16 +0300

    Update stroyka.api.ts

--- src/redux/stroyka.api.ts
+++ src/redux/stroyka.api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 import { API_TOKEN } from './api-key.env';
 
-const url = `https:/${API_TOKEN}/.mockapi.io/`
+const url = `https:/${API_TOKEN}.mockapi.io/`
 
 export const stroykaApi = createApi({
     reducerPath: 'stroykaApi',
